35998f69dda458904bd397dc4f5ade0e2fe5b475
Char <73309364+CharGoldenYT@users.noreply.github.com>
2026-04-09 15:36:58 -0400

    News Page
    Let's see if I can add the page to the topbar!

---
+++ src/routes/news/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="js">
+    import Topbar from '../../webpack/topbar.svelte';
+    const page = 'news';
+</script>
+<main>
+ <Topbar page={page}/>
+
+  <span> This is where the Solar Engine news would go...<br><br>IF I HAD ONE!</span>
+</main>
+
+<style>
+    span {
+        left: 50%;
+        top: 50%;
+        position: absolute;
+        transform: translate(-50%, -50%);
+    }
+</style>
